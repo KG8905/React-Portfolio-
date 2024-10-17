51c7cfc4f4b3761481eda997e94f678325b163f9
KG8905 <167706923+KG8905@users.noreply.github.com>
2024-10-16 20:34:42 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path'; // Import for resolving file paths
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'], // Externalize react-router-dom
-      output: {
-      
+    open: true
+  }
+})
