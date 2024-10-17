8060c2f33c10ef611e59a51d33c098bacc21dffb
KG8905 <167706923+KG8905@users.noreply.github.com>
2024-10-16 20:41:16 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,13 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+
+    export default {
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
+}
   }
 })
